19d4cf120e0b9eb26b651209401b6b4b538aab8c
Florent <flgallois@gmail.com>
2020-11-23 16:16:34 +0100

    FEATURE: CREATE HEADER AND TABLE

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="md-layout" style="">
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item">Login</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponent',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .md-layout{
+    line-height: 40px;
+  }
+
+  .md-layout-item {
+    height: 40px;
+    background-color:grey;
+  }
+
+</style>
